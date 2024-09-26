2baaf8c3752dec06948f4abb63356875b5c5e1ca
Julian F <juliandavidbf@gmail.com>
2024-09-26 11:42:43 -0400

    estrcutura proyecto

---
+++ src/app/pages/home/home-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'tasks',
+        component: TasksComponent
+      },
+      {
+        path: 'users',
+        loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
+      },
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
